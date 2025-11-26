e5e6e046729920b28924c1d991d6b6d2407175bd
Alex Adams <alex.adams@penten.com>
2025-11-26 15:32:46 +1100

    Update a_strdup and add a_strndup

--- include/astring.h
+++ include/astring.h
@@ -1,6 +1,9 @@
 #ifndef A_STRING_H
 #define A_STRING_H
 
+#include <stdlib.h>
+
 char* a_strdup(const char* s);
+char* a_strndup(const char* s, size_t n);
 
 #endif  // A_STRING_H
--- src/astring.c
+++ src/astring.c
@@ -2,10 +2,17 @@
 #include <string.h>
 
 char* a_strdup(const char* s) {
-    char* d = malloc(strlen(s) + 1);
+    char* d = malloc(sizeof(char) * (strlen(s) + 1));
     if (d == NULL) {
         return NULL;
     }
-    strcpy(d, s);
-    return d;
+    return strcpy(d, s);
+}
+
+char *a_strndup(const char *s, size_t n) {
+    char *d = malloc(sizeof(char) * (n + 1));
+    if (d == NULL) {
+        return NULL;
+    }
+    return strncpy(d, s, n);
 }
